0983f9bf73fae78a9d5a3960e71fb1b4a060d87d
Justin Settle <jus10@partlycloudy.org>
2024-02-11 22:00:44 -0500

    I guess this is alpha.  It works for me

---
+++ README.md
@@ -0,0 +1,3 @@
+# plane-db-builder
+
+Yes, I know I have to write documentation but this is as far as I've got so far! This is a basic python script which builds a custom version of the plane-alert-db csv file from the master sheet and the aircraft.csv.gz included in tar1090. Once set up with the query configs, you can run it anytime to generate updated files which can be used by planealert or upa.
--- plane-db-builder.py
+++ plane-db-builder.py
@@ -2,8 +2,8 @@
 
 import pandas as pd
 
-# infile is the supplments, data to be added
-infile = pd.read_csv("in.csv", header=0, dtype=str, index_col="$ICAO")
+# supplement is the additional ICAOs to be added
+supplement = pd.read_csv("supplement.csv", header=0, dtype=str, index_col="$ICAO")
 
 # aircraft list uses a ; instead of a comma
 aircraftlist = pd.read_csv(
@@ -26,8 +26,8 @@ aircraftlist = pd.read_csv(
 ).set_index("$ICAO")
 aircraftlist = aircraftlist.drop(["Zeros", "Six", "Eight", "Nine"], axis=1)
 
-infile.update(aircraftlist)
-# infile.to_csv("out.csv")
+# Patch the supplement list with data from the aircraft database
+supplement.update(aircraftlist)
 
 originaldb = pd.read_csv("plane-alert-db.csv", header=0, dtype=str, index_col="$ICAO")
 
@@ -39,6 +39,7 @@ aero = originaldb.query('Category == "Aerobatic Teams"')
 radiohead = originaldb.query('Category == "Radiohead"')
 gov = originaldb.query('Category == "Governments"')
 historic = originaldb.query('Category == "Historic"')
+dbcool = originaldb.query('Category == "Joe Cool"')
 
 # By tag; same idea
 # See the backticks on `$Tag 1`
@@ -48,22 +49,23 @@ big = originaldb.query('`$Tag 1` == "Absolute Unit"')
 # We can also add data from the aircraft.csv.gz
 cool = aircraftlist.query(
     '`$ICAO Type` == ["P51","B17","B25","B29","B52","B1","A1","A6", \
-    "F35","F15","F16","F18S","F18H","P38"]'
+    "F35","F15","F16","F18S","F18H","P38","AT43","AT44", \
+    "AT45","AT72","SHIP","U2","P3","E6","P8","EUFI","C5M"]'
 )
 
 customdb = pd.concat(
-    [infile, dictators, gasbags, aero, radiohead, gov, historic, big, cool]
+    [supplement, dictators, gasbags, aero, radiohead, gov, historic, big, cool]
 )
 customdb = customdb.sort_values(by="$ICAO")
-customdb.to_csv("out.csv")
+customdb.to_csv("plane-alert-db-custom.csv")
 
 # For HTML, drop the tags, categories and links
 htmldb = customdb.drop(
     ["#CMPG", "$Tag 1", "$#Tag 2", "$#Tag 3", "Category", "$#Link"], axis=1
 )
-htmldb.to_html("out.html")
+htmldb.to_html("table.html", show_dimensions=True, na_rep="")
 
-# Finally let's output planes.csv which is just the ICACO
+# Finally let's output planes.csv which is just the ICAO
 planesdb = htmldb.drop(["$Registration", "$Operator", "$Type", "$ICAO Type"], axis=1)
 # add an empty column so we have a , separator
 planesdb["Blank"] = ""
